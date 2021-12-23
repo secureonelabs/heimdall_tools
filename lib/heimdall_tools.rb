39dfd2f2e9a13029dcf34bfdbdc7d08b9c21f05e
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-12-23 16:23:45 -0500

    Change heimdall_tools to SAF CLI
    Signed-off-by: Camden Moors <66680985+camdenmoors@users.noreply.github.com>

--- lib/heimdall_tools.rb
+++ lib/heimdall_tools.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.expand_path(__dir__))
 require 'heimdall_tools/version'
-puts "NOTICE: heimdall_tools Ruby gem is deprecated. Please see instructions on how to convert to the new typescript version. https://github.com/mitre/heimdall_tools\n\n"
+puts "NOTICE: The heimdall_tools Ruby gem is deprecated. Please see instructions on how to convert to the new TypeScript SAF CLI. https://github.com/mitre/saf\n\n"
 
 module HeimdallTools
   autoload :Help, 'heimdall_tools/help'
